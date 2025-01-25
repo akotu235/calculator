92764b6eec30b25bb3ce89c9238b312d77a34c3f
akotu <akotu@protonmail.com>
2025-01-25 20:27:22 +0100

    Updated Main class

--- src/main/java/io/github/akotu235/calculator/Main.java
+++ src/main/java/io/github/akotu235/calculator/Main.java
@@ -1,16 +1,39 @@
 package io.github.akotu235.calculator;
 
+import io.github.akotu235.calculator.exception.reader.ReaderException;
 import io.github.akotu235.calculator.factory.DefaultOperationFactory;
 import io.github.akotu235.calculator.factory.OperationFactory;
+import io.github.akotu235.calculator.model.OperationData;
+import io.github.akotu235.calculator.model.Result;
+import io.github.akotu235.calculator.reader.ConsoleDataReader;
+import io.github.akotu235.calculator.reader.DataReader;
+import io.github.akotu235.calculator.reader.JsonFileDataReader;
 
 public class Main {
     public static void main(String[] args) {
         OperationFactory factory = new DefaultOperationFactory();
         Calculator calculator = new Calculator(factory);
 
-        System.out.println("Add: " + calculator.calculate("add", 10, 5)); // 15.0
-        System.out.println("Subtract: " + calculator.calculate("subtract", 10, 5)); // 5.0
-        System.out.println("Multiply: " + calculator.calculate("multiply", 10, 5)); // 50.0
-        System.out.println("Divide: " + calculator.calculate("divide", 10, 5)); // 2.0
+        DataReader dataReader = getDataReader(args);
+
+        try {
+            OperationData operationData = dataReader.readData();
+            Result result = calculator.calculate(operationData);
+            printResult(result);
+        } catch (ReaderException e) {
+            printResult(new Result(e.getMessage()));
+        }
+    }
+
+    private static DataReader getDataReader(String[] args) {
+        return args.length > 0 ? new JsonFileDataReader(args[0]) : new ConsoleDataReader();
+    }
+
+    private static void printResult(Result result) {
+        if (result.isSuccess()) {
+            System.out.println("Result: " + result);
+        } else {
+            System.err.println("Error: " + result);
+        }
     }
 }
